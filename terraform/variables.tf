9813b45d664f735d9dc1031093296036265eab26
Abhigyan <avigyan2212@gmail.com>
2026-07-26 10:30:30 +0530

    Declare ssh_public_key variable in Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "instance_type" {
   default = "t3.small"
   type    = string
 }
+
+variable "ssh_public_key" {
+  description = "The public SSH key for EC2 access"
+  type        = string
+}
